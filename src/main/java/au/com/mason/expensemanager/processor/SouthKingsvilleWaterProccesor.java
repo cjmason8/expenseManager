b8f33a66f70cd2a4e365bd54a5dd607517168abb
Chris Mason <cjmason8@gmail.com>
2021-09-03 15:01:23 +1000

    Update SouthKingsville Water

--- src/main/java/au/com/mason/expensemanager/processor/SouthKingsvilleWaterProccesor.java
+++ src/main/java/au/com/mason/expensemanager/processor/SouthKingsvilleWaterProccesor.java
@@ -1,10 +1,16 @@
 package au.com.mason.expensemanager.processor;
 
-import java.io.BufferedInputStream;
-import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import java.time.format.DateTimeFormatterBuilder;
 
 import javax.mail.BodyPart;
 import javax.mail.Message;
@@ -14,6 +20,8 @@ import org.springframework.stereotype.Component;
 
 import au.com.mason.expensemanager.domain.Document;
 import au.com.mason.expensemanager.domain.RefData;
+import au.com.mason.expensemanager.pdf.PdfReader;
+import au.com.mason.expensemanager.util.CollectionUtil;
 
 @Component
 public class SouthKingsvilleWaterProccesor extends Processor {
@@ -26,31 +34,38 @@ public class SouthKingsvilleWaterProccesor extends Processor {
 			MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
 			int count = mimeMultipart.getCount();
 			LocalDate dueDate = null;
-			String amount = null;
-			Document document = null;
+			String[] amount = new String[1];
+			DateTimeFormatter dueDateFormatter = new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("dd MMM yyyy").toFormatter();
 			for (int i = 0; i < count; i++) {
 				BodyPart bodyPart = mimeMultipart.getBodyPart(i);
 				if (bodyPart.isMimeType("text/html")) {
 					body = (String) bodyPart.getContent();
-					amount = body.substring(body.indexOf(">$") + 2, body.indexOf("<", body.indexOf(">$"))).trim();
-					int startIndex = body.indexOf(">By") + 4;
-		            String dueDateString = body.substring(startIndex, body.indexOf("<", startIndex)).trim().toLowerCase();
-		            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy").localizedBy(Locale.ENGLISH);
-		            String newDueDateString = dueDateString.substring(0, dueDateString.indexOf(" "))
-		            		+ " " + dueDateString.substring(dueDateString.indexOf(" ") + 1, dueDateString.indexOf(" ") + 2).toUpperCase()
-		            		+ dueDateString.substring(dueDateString.indexOf(" ") + 2); 
-		            dueDate = LocalDate.parse(newDueDateString, formatter);
-		            
-		            startIndex = body.indexOf("<a href=\"https://view") + 9;
-		            String url = body.substring(startIndex, body.indexOf("\"", startIndex));
+					String url = body.substring(body.indexOf("https://viewpoint"), body.indexOf("\"", body.indexOf("https://viewpoint")));
+					int start = body.indexOf(">By") + 4;
+					dueDate = LocalDate.parse(body.substring(start, body.indexOf("<", start)), dueDateFormatter);
 
-					BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+					HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).followRedirects(Redirect.ALWAYS)
+							.build();
+
+					HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).timeout(Duration.ofMinutes(1))
+							.GET().build();
+
+					HttpResponse<byte[]> response = client.send(request, BodyHandlers.ofByteArray());
 					
-		        	DateTimeFormatter fileDateFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
-		        	String fileName = "SouthKingsvilleWater-" + fileDateFormatter.format(dueDate) + ".pdf";
-					document = documentService.createDocumentFromEmailForExpense(in.readAllBytes(), fileName);
+					String content = PdfReader.extract(response.body());
 					
-					updateExpense(refData, dueDate, amount, document);
+					CollectionUtil.splitAndConvert(content, "\n").stream().forEach(line -> {
+						if (line.indexOf("PLEASE PAY") != -1) {
+							amount[0] = line.substring(line.indexOf("$") + 1);
+						}
+					});
+					
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy");
+					
+					String fileName = "SouthKingsvilleWater-" + formatter.format(dueDate) + ".pdf";
+					Document document = documentService.createDocumentFromEmailForExpense(response.body(), fileName);
+
+					updateExpense(refData, dueDate, amount[0], document, null);
 				}
 			}
 		}
