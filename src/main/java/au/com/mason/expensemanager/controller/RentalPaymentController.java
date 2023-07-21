70a7255dd10356245622fd850516080e2c49acc4
Chris Mason <chris.mason@stocksoftware.com.au>
2023-07-21 16:41:03 +1000

    fix rental display issue

--- src/main/java/au/com/mason/expensemanager/controller/RentalPaymentController.java
+++ src/main/java/au/com/mason/expensemanager/controller/RentalPaymentController.java
@@ -40,12 +40,12 @@ public class RentalPaymentController extends BaseController<RentalPaymentDto, Re
 			year = (LocalDate.now().getMonth().getValue() <= 6) ? LocalDate.now().getYear() - 1 : LocalDate.now().getYear();
 			
 			if (LocalDate.now().getMonth().getValue() <= 6) {
-				startDate = LocalDate.of(year, 7, 1);
-				endDate = LocalDate.of(year + 1, 6, 30);
-			}
-			else {
 				startDate = LocalDate.of(year - 1, 7, 1);
 				endDate = LocalDate.of(year, 6, 30);
+			}
+			else {
+				startDate = LocalDate.of(year, 7, 1);
+				endDate = LocalDate.of(year + 1, 6, 30);
 			}
 		}
 		else {
